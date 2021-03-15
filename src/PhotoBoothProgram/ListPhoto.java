000065f0496cca30b84ccb344d639bfeb0a15777
Alex <lomincha@mail.gvsu.edu>
2021-03-15 14:13:34 -0400

    GUI implementation

---
+++ src/PhotoBoothProgram/ListPhoto.java
@@ -0,0 +1,27 @@
+//package PhotoBoothProgram;
+//
+//        import javax.swing.*;
+//        import javax.swing.table.AbstractTableModel;
+//        import java.io.*;
+//        import java.lang.ref.PhantomReference;
+//        import java.text.DateFormat;
+//        import java.text.ParseException;
+//        import java.text.SimpleDateFormat;
+//        import java.util.*;
+//
+//public class ListPhoto extends AbstractTableModel {
+//
+//    private int display;
+//
+//    private static final int colEditScreen = 2;
+//
+//    private static final int colPhotoScreen = 1;
+//
+//    private static final int colGalleryScreen = 3;
+//
+//    private ArrayList<Photo> listPhotos;
+//
+//    private ArrayList<Photo> filteredPhotos;
+//
+//
+//}
