22a45c91479e066979834e904a83428c265f532a
Nathan Funckes <49565381+funckytown1@users.noreply.github.com>
2021-04-02 11:19:34 -0400

    Add files via upload

---
+++ opencv_concept.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
