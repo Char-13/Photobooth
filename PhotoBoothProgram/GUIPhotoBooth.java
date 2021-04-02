ee871945d832b424ec5cea5652f5aa450384df72
Char-13 <55597096+Char-13@users.noreply.github.com>
2021-04-02 10:32:29 -0400

    Update GUIPhotoBooth.java

--- PhotoBoothProgram/GUIPhotoBooth.java
+++ PhotoBoothProgram/GUIPhotoBooth.java
@@ -1,75 +1,79 @@
 package PhotoBoothProgram;
 
-import javax.imageio.ImageIO;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.text.ParseException;
 
 
 public class GUIPhotoBooth extends JFrame implements ActionListener {
 
-    // JMenu bar that creates the possible menus in the program.
+    /** JMenu bar that creates the possible menus in the program. **/
     private JMenuBar menus;
 
-    //Two menus at the top right of the program that allows the user to interact with the program.
+    /**The menu at the top right of the program that allows the user to interact with the program. **/
     private JMenu fileMenu;
-    private JMenu actionMenu;
 
-    // All the menu options available to the user when they run the program.
-    private JButton savePhoto;
-    private JButton takePhoto;
+    /**
+     All the menu options available to the user when they run the program
+     **/
     private JMenuItem EditorMode;
     private JMenuItem SavedGallery;
     private JMenuItem exitItem;
 
-    //    private ListPhoto PList;
-    private JPanel panel;
-    private JPanel panel2;
 
+
+    private JPanel panel;
+    //private JPanel panel2;
+
+
+    /** GUIPhotoBooth constructor that creates a GUI for the program. **/
     public GUIPhotoBooth(){
+
+        /**creates the menu and menu items. **/
         menus = new JMenuBar();
         fileMenu = new JMenu("File");
         exitItem = new JMenuItem("Exit");
         EditorMode = new JMenuItem("Editor Mode");
-        SavedGallery= new JMenuItem("Saved Gallery");
+        SavedGallery= new JMenuItem("Open Photo");
 
-
+        /**adds the menu items to the gui **/
         fileMenu.add(EditorMode);
         fileMenu.add(SavedGallery);
         fileMenu.add(exitItem);
 
+        //adds the fileMenu to the gui
         menus.add(fileMenu);
-        //menus.add(actionMenu);
 
+        //creates action listeners for the file menu items.
         fileMenu.addActionListener(this);
-        //actionMenu.addActionListener(this);
-        //takePhoto.addActionListener(this);
-        //savePhoto.addActionListener(this);
         exitItem.addActionListener(this);
         EditorMode.addActionListener(this);
         SavedGallery.addActionListener(this);
 
+        //sets menus variable to a JMenuBar
         setJMenuBar(menus);
+        //closes JFrame on system exit
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //creates new panel variable.
         panel = new JPanel();
+        //adds the panel variable and centers it in the middle of the gui.
         add(panel, BorderLayout.CENTER);
+
     }
 
-
+    //action performed method that applies logic to all click events in the gui.
     public void actionPerformed(ActionEvent e){
 
-        Object comp = e.getSource();
+        //Object comp = e.getSource();
 
-        if (e.getSource() == EditorMode){
-            editorScreen gui = new editorScreen();
-
-        }
-
+//        if (e.getSource() == EditorMode){
+//           // editorScreen gui = new editorScreen();
+//
+//        }
+        // similar logic to the editor screen right as we are testing this feature and awaiting for camera
+        // implementation.
         if (e.getSource() == SavedGallery) {
             JFileChooser chooser = new JFileChooser();
             int status = chooser.showOpenDialog(null);
@@ -80,29 +84,18 @@ public class GUIPhotoBooth extends JFrame implements ActionListener {
                 Image newImg = scaledImage.getScaledInstance(400,400, Image.SCALE_SMOOTH);
                 ImageIcon lastImage = new ImageIcon(newImg);
                 JLabel label = new JLabel(lastImage);
+               // newPhoto.setDisplay(2);
                 //new editorScreen(lastImage);
                 panel.add(label);
             }
         }
-
-//        if (e.getSource() == takeVideo && newPhoto.getDisplay() == 1){
-//
-//        }
-//
-//        if (e.getSource() == takePhoto){
-//
-//        }
-
-        if (e.getSource() == savePhoto){
-
-        }
-
+        
+        //exits the program when the user hits exit on the file menu.
         if (e.getSource() == exitItem){
             System.exit(0);
         }
 
     }
-
 
     public static void main(String[] args){
         GUIPhotoBooth gui = new GUIPhotoBooth();
